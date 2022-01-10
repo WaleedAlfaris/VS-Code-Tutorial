f55b19a9d6e0893a0a0a3e35b15dc4df6643bb01
WaleedAlfaris <waleed.alfaris@whiteshieldpartners.com>
2022-01-10 17:49:11 +0400

    rest of commit

--- refactoring copy.py
+++ refactoring copy.py
@@ -3,3 +3,20 @@ from datetime import datetime as dt
 from folder_1.getters import get_tail, get_user_age
 
 def main():
+
+    user_birthdate = input('Enter your birthday as MM/DD/YYYY\n')
+    today = dt.today()
+
+    happy_birthday, user_age = get_user_age(user_birthdate, today)
+
+    tail = get_tail(user_age)
+
+    if happy_birthday == True:
+        print(f"Happy {user_age}{tail} Birthday!\n")
+    else:
+        print(f'You are {user_age} years old.\n')
+
+
+
+if __name__ == '__main__':
+    main()
