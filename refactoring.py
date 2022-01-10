9612649b501e20d560b233eac3b429abf62879ae
WaleedAlfaris <waleed.alfaris@whiteshieldpartners.com>
2022-01-10 17:46:41 +0400

    dont stage like this

---
+++ refactoring.py
@@ -0,0 +1,40 @@
+from datetime import datetime as dt
+
+def main():
+
+    user_birthdate = input('Enter your birthday as MM/DD/YYYY\n')
+    today = dt.today()
+
+    happy_birthday = False
+    user_birthdate = dt.strptime(user_birthdate, '%m/%d/%Y')
+    user_age = (today.year - user_birthdate.year) - 1
+    if today.month > user_birthdate.month:
+        user_age += 1
+    if user_birthdate.month == today.month:
+        if user_birthdate.day == today.day:
+            happy_birthday = True
+            user_age += 1
+        if user_birthdate.day > today.day:
+            user_age += 1
+
+    age_tails_dict = {
+        'th': ['0', '4', '5', '6', '7', '8', '9'],
+        'st': ['1'],
+        'nd': ['2'],
+        'rd': ['3']
+    }
+
+    if 1 <= user_age <= 19:
+        tail = 'th'
+    else:
+        for age_tail in age_tails_dict.items():
+            if str(user_age)[-1] in age_tail[1]:
+                tail = age_tail[0]
+
+    if happy_birthday == True:
+        print(f"Happy {user_age}{tail} Birthday!\n")
+    else:
+        print(f'You are {user_age} years old.\n')
+
+if __name__ == '__main__':
+    main()
